b0a0ab51a19287ecccbf2c370b6b02b054823c72
Ricardo Arana Reyes <raranare@everis.com>
2020-09-19 16:41:40 -0500

    ngmodel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './models/persona.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'registros';
+  persona: Persona;
+  esDev: boolean;
+  esAnalista: boolean;
+  esTeamLeader: boolean;
+  estadoCivil: string;
+
+  constructor() {
+      this.persona = { nombre: '', apellido: ''};
+  }
+
+  enviarDatos(forma: any) {
+    if(forma.valid) {
+      console.log('Formulario valido');
+    } else {
+      console.log('Formulario invalido');
+    }
+    console.log(forma);
+    
+  }
 }
